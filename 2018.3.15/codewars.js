aa1d443d37f77a131efebe6c2854f4987cb40fe8
Stephen <2655461060@qq.com>
2018-03-15 23:53:39 +1000

    2018.3.15 11:53 更新

---
+++ 2018.3.15/codewars.js
@@ -0,0 +1,11 @@
+// 给定两个字符串, 返回第二个字符串在第一个字符串中的个数
+var countCombinations = function(string, key) {
+    var result = 0
+    for (var i = 0; i < string.length; i++) {
+        var letter = string[i]
+        if (key.includes(letter)) {
+            
+        }
+    }
+
+}
