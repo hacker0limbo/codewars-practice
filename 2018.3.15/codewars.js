89ac8456aa7450c4397f6326ceea009a058df7fe
Stephen <2655461060@qq.com>
2018-03-16 01:24:40 +1000

    2018.3.15 23:28 更新

--- 2018.3.15/codewars.js
+++ 2018.3.15/codewars.js
@@ -1,11 +1,15 @@
-// 给定两个字符串, 返回第二个字符串在第一个字符串中的个数
-var countCombinations = function(string, key) {
-    var result = 0
-    for (var i = 0; i < string.length; i++) {
-        var letter = string[i]
-        if (key.includes(letter)) {
-            
-        }
+// 给定一个字符串, 返回中间的字符
+var getMiddle = function(str) {
+    var result = ''
+    var l = str.length
+    if (l % 2 == 1) {
+        result += str[Math.floor(l/2)]
     }
-
+    else {
+        result += str[l/2 - 1] + str[l/2]
+    }
+    return result
 }
+
+// getMiddle("test") should return "es"
+// getMiddle("testing") should return "t"
