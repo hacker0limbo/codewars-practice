45e1dc94c80d3c108c6a283fd019b5e706a8266d
Stephen <2655461060@qq.com>
2018-03-18 01:42:43 +1000

    2018.3.17 1:42 更新

--- 2018.3.16/codewars.js
+++ 2018.3.16/codewars.js
@@ -72,15 +72,101 @@ var spinWords = function(str) {
 // 给定一个数字, 不断将各个位数的数字相加, 直到最后加到为个位数, 返回最后结果
 // QUESTION: 时间复杂度太高
 var digital_root = function(nums) {
-    var result = 0
-    var nums = String(nums)
-    while (nums.length > 1) {
-        for (var i = 0; i < nums.length; i++) {
-            var num = Number(nums[i])
-            result += num
+    var numbers = String(nums)
+    while (numbers.length > 1) {
+        var result = 0
+        for (var i = 0; i < numbers.length; i++) {
+            var number = Number(numbers[i])
+            result += number
         }
-        nums = String(result)
+        numbers = String(result)
     }
-    return Number(result)
+    return Number(numbers)
 }
-// console.log(digital_root(493193));
+// console.log(digital_root(16));
+// console.log(digital_root(942));
+
+
+// 给定一个字符串, 按照字母顺序排序, 同时要大写的字母在前面
+// FIXME: 未完成
+var findChildren = function(str) {
+    var strArray = str.split('')
+}
+
+
+// 给定一个字符串, 将里面的'WUB'去掉, 并且每个之间用空格隔开
+var songDecoder = function(str) {
+    var strArray = str.split('WUB')
+    var arr = []
+    strArray.forEach(function(str) {
+        if (str != '') {
+            arr.push(str)
+        }
+    })
+    return arr.join(' ')
+}
+console.log(songDecoder("AWUBWUBWUBBWUBWUBWUBC"));
+
+// REVIEW: 有一个很优雅的方法:
+/*
+// // NOTE:
+可以使用 Boolean() 函数将之转换为布尔值
+    undefined
+    null
+    -0
+    +0
+    NaN
+    ‘’（空字符串）
+因此可以使用 filter() 函数:
+    它的参数是一个函数，所有数组成员依次执行该函数，
+    返回结果为 true 的成员组成一个新数组返回。该方法不会改变原数组。
+*/
+var songDecoder = function(song) {
+    return song.split('WUB').filter(Boolean).join(' ');
+}
+
+
+// 给定一个字符串, 将里面的每一个字符用字母表里面的 +13 位以后的那个替换掉
+var rot13 = function(str) {
+    var result = ''
+    var lowerLetters = 'abcdefghijklmnopqrstuvwxyz'
+    var upperLetters = lowerLetters.toUpperCase()
+
+    for (var i = 0; i < str.length; i++) {
+        var letter = str[i]
+        if (lowerLetters.includes(letter)) {
+            var index = lowerLetters.indexOf(letter)
+            var newIndex = (index + 13) % 26
+            var newLetter = lowerLetters[newIndex]
+            console.log(index, newIndex);
+            result += newLetter
+        }
+        else if (upperLetters.includes(letter)) {
+            var index = upperLetters.indexOf(letter)
+            var newIndex = (index + 13) % 26
+            var newLetter = upperLetters[newIndex]
+            result += newLetter
+        }
+        else {
+            result += letter
+        }
+    }
+    return result
+}
+console.log(rot13("test"));
+
+
+// 给一个数组, 找到一个与其他元素不同的元素
+// // FIXME: 未完成
+var findUniq = function(arr) {
+    var result = arr[0]
+    var last = arr[arr.length-1]
+    for (var i = 0; i < arr.length; i++) {
+        var each = arr[i]
+        if (result != each && last != each) {
+            result = each
+            break
+        }
+    }
+    return result
+}
